5c1e06c6c1c4dfa004e4cde4cb52674b723bb190
Vlad7 <vd.popov7@gmail.com>
2024-08-09 16:07:38 +0400

    test commit

---
+++ src/test/java/first_test.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class first_test {
+
+    @Test
+    void succesfulLoginTest() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = "firefox";
+
+
+        // Открыть форму авторизации
+        open("https://qa.guru/cms/system/login");
+        // Ввести почту
+        $("[name=email]").setValue("qagurubot@gmail.com");
+        // ввести пароль
+        $("[name=password]").setValue("qagurupassword");
+        // Нажать кнопку войти
+        $(".btn-success").click();
+        // Нажать на кнопку "Личный кабинет"
+        $(".main-header__login").click();
+        // Проверить успешную авторизацию
+        $(".logined-form").shouldHave(text("QA_GURU_BOT")).click();
+    }
+
+
+}
