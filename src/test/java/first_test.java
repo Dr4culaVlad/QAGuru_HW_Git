8de9d7c2bb22494738bf4b3ad247474ece673835
Dr4culaVlad <delaidobra@gmail.com>
2024-08-09 16:10:12 +0400

    Update first_test.java

--- src/test/java/first_test.java
+++ src/test/java/first_test.java
@@ -14,7 +14,7 @@ public class first_test {
 
 
         // Открыть форму авторизации
-        open("https://qa.guru/cms/system/login");
+        open("https://qa.guru/cms/system/logif");
         // Ввести почту
         $("[name=email]").setValue("qagurubot@gmail.com");
         // ввести пароль
